6741cd478ee0bca35688ae5edc876af9f84016d3
Affandes <affandes@uin-suska.ac.id>
2021-11-29 10:02:37 +0700

    Initial project

---
+++ svelte.config.js
@@ -0,0 +1,16 @@
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter(),
+		appDir: "app",
+		paths: {
+			base: process.env.NODE_ENV === "production" ? "" : "",
+		},
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte'
+	}
+};
+
+export default config;
